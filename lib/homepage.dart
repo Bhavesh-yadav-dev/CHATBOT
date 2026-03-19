6c49e690e7131c7e09078b202e37b6fcbc0d07dc
rerturnError09 <bhaveshyadavlinkdin19@gmail.com>
2026-03-19 21:03:45 +0530

    ui and are now properly seen in screen

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
+import 'package:gpt_markdown/gpt_markdown.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
@@ -15,53 +16,59 @@ class _HomepageState extends State<Homepage> {
   String res = "";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "CHAT APP ",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            "CHAT APP ",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          
         ),
-        centerTitle: true,
-        elevation: sqrt1_2,
-      ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: TextField(
-                controller: userinput,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  label: Text("enter here"),
-                  hint: Text("hey, what you think just ask me"),
+        body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 20,
+                ),
+              child: GptMarkdown(res, style: const TextStyle(fontSize: 20)),
+            ),
+          ),
+
+           Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: TextField(
+              controller: userinput,
+              decoration: InputDecoration(
+                hintText: "Ask me anything...",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    if (userinput.text.isNotEmpty) {
+                      getresponse();
+                      userinput.clear(); 
+                    }
+                  },
+                  icon: const Icon(Icons.send),
                 ),
               ),
             ),
-
-            ElevatedButton(
-              onPressed: () {
-                if (userinput.text.isNotEmpty) {
-                  getresponse();
-                }
-              },
-              child: Text("send your throught"),
-            ),
-
-            Text(res, style: TextStyle(fontSize: 20)),
-          ],
-        ),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 
   void getresponse() async {
-    String apikey = "";
+    String apikey = "AIzaSyB2tORN8Kmo4sWngYSVsj-8f280QuwtZkY";
     String url =
         "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=$apikey";
 
@@ -77,9 +84,16 @@ class _HomepageState extends State<Homepage> {
     var response = await http.post(Uri.parse(url), body: jsonEncode(parambody));
 
     if (response.statusCode == 200) {
-      print(response.body);
+      var decodedData = jsonDecode(response.body);
+      String aiMessage =
+          decodedData['candidates'][0]['content']['parts'][0]['text'];
+
+      setState(() {
+        res = aiMessage;
+      });
     } else {
-      print(response.statusCode);
+      print("Error Code: ${response.statusCode}");
+      print("Error Body: ${response.body}");
     }
   }
 }
